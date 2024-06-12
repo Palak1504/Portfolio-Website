a3127e911217727194440838eb2cb6d0f8b8abd4
Palak Patidar <palakpatidar1504@gmail.com>
2024-06-12 12:51:45 +0530

    About Change

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
       <div className='container about__container'>
         <div className="about__me">
           <div className='about__me_image'>
-            <img src={ME} />
+            <img src={ME} alt =""/>
           </div>
         </div>
       <div className='about__content'>
