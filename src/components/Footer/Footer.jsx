450ab70fc1d7a5654157fafcf6aa5192722f23a2
Palak Patidar <palakpatidar1504@gmail.com>
2024-06-12 13:08:52 +0530

    Link Changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,9 +6,9 @@ import {FiInstagram} from "react-icons/fi";
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>PALAK</a>
+      <a href="src\components\Header\Header.jsx" className='footer__logo'>PALAK</a>
       <ul className='personal_links'>
-        <li><a href="#">Home</a></li>
+        <li><a href="src\components\Header\Header.jsx">Home</a></li>
         <li><a href="#about">About</a> </li>
         <li><a href="#skills">Skills</a> </li>
         <li><a href="#projects">Projects</a> </li>
